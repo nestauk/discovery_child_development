948a56738f606c5a601dc21ec306f13d678fab80
beingkk <kandersk@gmail.com>
2023-10-03 17:17:22 +0100

    adding bigquery utils

---
+++ discovery_child_development/notebooks/patents/test.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     comment_magics: true
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: discovery_child_development
+#     language: python
+#     name: python3
+# ---
+
+# # Testing connection with BigQuery
+
+# +
+from discovery_child_development.utils import bigquery
+
+client = bigquery.create_client()
+
+# Define the SQL query
+sql = """
+SELECT *
+FROM `patents-public-data.patents.publications`
+LIMIT 10;
+"""
+
+# Execute the query
+query_job = client.query(sql)
+
+# Fetch the results
+results = query_job.result()
+# -
+
+next(results)
