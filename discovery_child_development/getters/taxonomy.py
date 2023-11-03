44a5637193f1a580664c533e01cb0896edf6fde8
RFOxbury <rosamundoxbury@gmail.com>
2023-11-03 11:39:32 +0000

    Add docstrings

--- discovery_child_development/getters/taxonomy.py
+++ discovery_child_development/getters/taxonomy.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 
 from discovery_child_development import PROJECT_DIR, logging
@@ -11,7 +10,18 @@ SHEET_ID = PARAMS["taxonomy"]["sheet_id"]
 SHEET_NAME = PARAMS["taxonomy"]["sheet_name"]
 
 
-def clean_string(s):
+def clean_string(s: str):
+    """Custom string cleaning function for the taxonomy.
+
+    Because the taxonomy data was edited manually in Google Sheets, we need to clean up
+    normal human errors like adding one too many spaces, inconsistent capitalisation, typos etc.
+
+    Args:
+        s (str): A string to be cleaned eg "Statistical methods ?"
+
+    Returns:
+        str: A cleaned string eg "statistical methods"
+    """
     s = s.replace(
         "?", ""
     )  # Remove question marks (do this *before* removing trailing spaces)
@@ -22,10 +32,25 @@ def clean_string(s):
 
 
 def get_taxonomy(
-    sheet_id=SHEET_ID,
-    sheet_name=SHEET_NAME,
-    works_threshold=N_WORKS,
-):
+    sheet_id: str = SHEET_ID,
+    sheet_name: str = SHEET_NAME,
+    works_threshold: int = N_WORKS,
+) -> pd.DataFrame:
+    """
+    Retrieves and processes taxonomy data from a specified Google Sheet.
+
+    The data is filtered to include only relevant entries with
+    a number of works above a given threshold. It also cleans the 'Sub category'
+    column using a custom string cleaning function (defined above).
+
+    Parameters:
+        sheet_id (str): The unique identifier for the Google Sheets file. Defaults to SHEET_ID.
+        sheet_name (str): The name of the individual sheet within the Google Sheets file. Defaults to SHEET_NAME.
+        works_threshold (int): The minimum number of works for the taxonomy entries to be included. Defaults to N_WORKS.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the cleaned and filtered taxonomy data.
+    """
     taxonomy_data = google_utils.access_google_sheet(sheet_id, sheet_name)
 
     # convert this column so that we can filter on it more easily
