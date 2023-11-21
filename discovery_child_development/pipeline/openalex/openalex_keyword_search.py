a08058d189984932e6ea7ef8c8f95e7e659fa119
RFOxbury <rosamundoxbury@gmail.com>
2023-11-21 18:14:38 +0000

    Search abstract and title only, not full works.

--- discovery_child_development/pipeline/openalex/openalex_keyword_search.py
+++ discovery_child_development/pipeline/openalex/openalex_keyword_search.py
@@ -6,10 +6,11 @@ from metaflow import FlowSpec, S3, step, Parameter, retry, batch
 from nesta_ds_utils.loading_saving import S3 as nesta_s3
 import requests
 from typing import List
+import time
 
 from discovery_child_development import S3_BUCKET, config
 
-API_ROOT = "https://api.openalex.org/works?search=(child OR infant OR baby OR prenatal OR pregnancy) AND "
+API_ROOT = "https://api.openalex.org/works?search=(abstract:(child OR infant OR baby OR prenatal OR pregnancy) AND KEYWORD) OR (title:(child OR infant OR baby OR prenatal OR pregnancy) AND KEYWORD)"
 S3_PATH = "metaflow"
 YEARS = config["openalex_years"]
 KEYWORDS = config["openalex_keywords"]
@@ -17,23 +18,56 @@ KEYWORDS = config["openalex_keywords"]
 load_dotenv()
 
 
-def generate_queries(root=API_ROOT, keywords=KEYWORDS, years=YEARS):
+def generate_queries(
+    root: str = API_ROOT, keywords: List[str] = KEYWORDS, years: List[int] = YEARS
+) -> List[str]:
+    """
+    Generates a list of API queries based on given keywords and years.
+
+    This function constructs queries by combining a root API endpoint with
+    specified keywords and a filter for publication years. Each query is
+    formed by appending a keyword and a year filter to the root endpoint.
+
+    Args:
+    root (str): The root URL or endpoint of the API.
+    keywords (List[str]): A list of keywords to include in the queries.
+    years (List[int]): A list of years to filter the publications by.
+
+    Returns:
+    List[str]: A list of complete API query strings.
+    """
     queries = []
     for k in keywords:
+        temp_root = root.replace("KEYWORD", k)
         for year in years:
-            queries.append(f"{root}{k}&filter=publication_year:{year}")
+            queries.append(f"{temp_root}&filter=publication_year:{year}")
     return queries
 
 
-def api_generator(query):
-    page_one = query
-    print(f"Running API query {page_one}")
-    total_results = requests.get(page_one).json()["meta"]["count"]
+def api_generator(query: str) -> List[str]:
+    """Generates a list of all URLs needed to completely collect
+    all works relating to the list of concepts.
+
+    Because only a certain number of results can be returned as one page, you
+    may need to access multiple pages in order to get all the hits for a
+    single query. This function takes a single query as input and by calculating
+    how many results that query would return, then dividing that by 200, it figures
+    out how many pages you will need to query. The result is a list where every
+    item is the same API query, but crucially the length is the number of pages
+    needed. That means that in the metaflow below, this function can be used in
+    cursor paging.
+
+    Args:
+        query : the API query. This was produced by `generate_queries()` above.
+
+    Returns:
+        all_pages: list of pages required to return all results
+    """
+    print(f"Running API query {query}")
+    total_results = requests.get(query).json()["meta"]["count"]
     print(f"Total number of hits: {total_results}")
     number_of_pages = -(total_results // -200)  # ceiling division
-    all_pages = [
-        f"{page_one}&per-page=200&cursor=" for _ in range(1, number_of_pages + 1)
-    ]
+    all_pages = [f"{query}&per-page=200&cursor=" for _ in range(1, number_of_pages + 1)]
     return all_pages
 
 
@@ -62,7 +96,7 @@ class OpenAlexFlow(FlowSpec):
         self.next(self.retrieve_data, foreach="merged")
 
     @retry()
-    @batch(cpu=2, memory=48000)
+    # @batch(cpu=2, memory=48000)
     @step
     def retrieve_data(self):
         """Returns all results of the API hits"""
@@ -82,6 +116,7 @@ class OpenAlexFlow(FlowSpec):
             except:
                 print(f"Failure for query: {query}")
                 pass
+            time.sleep(2)
         filename = f"openalex_keywords_{self.production}.json"
 
         # Specify location to save the file within the bucket
