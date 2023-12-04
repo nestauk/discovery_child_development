12a2484c4437da20c379240cf61da30a21da63fd
beingkk <kandersk@gmail.com>
2023-12-04 15:01:22 +0000

    labelling patents

--- discovery_child_development/notebooks/labelling/run_relevancy_labelling_test_patents.py
+++ discovery_child_development/notebooks/labelling/run_relevancy_labelling_test_patents.py
@@ -27,41 +27,51 @@ from discovery_child_development.utils.labelling_utils import (
     create_examples_string,
 )
 
+from discovery_child_development.getters import patents
+from discovery_child_development.utils import keywords as kw
+
 PATH_TO_PROMPTS = (
     PROJECT_DIR / "discovery_child_development/notebooks/labelling/prompts/relevance"
 )
 PATH_TO_CATEGORIES = PATH_TO_PROMPTS / "categories.json"
 PATH_TO_MESSAGE_PROMPT = PATH_TO_PROMPTS / "prompt.json"
 PATH_TO_FUNCTION = PATH_TO_PROMPTS / "function.json"
-OUTPUT_FILENAME = (
+PATH_TO_EXAMPLES = PATH_TO_PROMPTS / "examples.jsonl"
+OUTPUT_FILEPATH = (
     PROJECT_DIR / "discovery_child_development/notebooks/labelling/data/relevance"
 )
-
-TEST_TEXTS = [
-    # 0: relevant
-    "A fun activity for babies aged 3-6 months to help development and language learning. Try blowing bubbles with your baby and see how they react. Talk to them about what they're seeing.",
-    # 1: non-relevant (child is too old)
-    "A fun activity for 6 year old children to help development and language learning. Try blowing bubbles with your child and see how they react. Talk to them about what they're seeing.",
-    # 2: non-relevant (non human)
-    "A fun activity for a piglet to help development and learning. Try blowing bubbles with your little one and see how they react. Talk to them.",
-    # 3: unclear (age not specified)
-    "A fun activity for a child to help development and learning. Try blowing bubbles and see how they react. Talk to them.",
-    # 4: not relevant (child is too old)
-    "Developing Middle School Students' AI Literacy. In this experience report, we describe an AI summer workshop designed to prepare middle school students to become informed citizens and critical consumers of AI technology and to develop their foundational knowledge and skills to support future endeavors as AI-empowered workers. The workshop featured the 30-hour Developing AI Literacy or DAILy curriculum that is grounded in literature on child development, ethics education, and career development. The participants in the workshop were students between the ages of 10 and 14; 87% were from underrepresented groups in STEM and Computing.",
-    # 5: unclear (age not specified)
-    "Diverse methods are emerging as effective strategies for gathering high-frequency data on child development, integrating various data sources ranging from telephone interviews to noninvasive biomarker readings. This comprehensive approach offers a more holistic understanding of children's health and developmental progress.",
-    # 6:relevant
-    "Multimodal approaches have been shown to be a promising way to collect data on child development at high frequency, combining different data inputs (from phone surveys to signals from noninvasive biomarkers) to understand children’s health and development outcomes more integrally from multiple perspectives. We carried out a mixed study based on a transversal descriptive analysis and a longitudinal prospective analysis in Malawi. In each village, children were sampled to participate in weekly sessions in which data signals were collected through wearable devices (electrocardiography [ECG] hand pads and electroencephalography [EEG] headbands). During the implementation, 82 EEG/ECG data entry points were collected across four villages. The sampled children for EEG/ECG were 0 to 5 years old.",
-]
+OUTPUT_FILENAME = "relevance_patents"
 
 
 async def main() -> None:
     """Fetch prompts and run the classifier"""
 
-    # Fetch the data to be categorised (replace with real data)
-    texts_df = pd.DataFrame(
-        data={"text": TEST_TEXTS, "id": list(range(len(TEST_TEXTS)))}
-    )
+    # Fetch the data to be categorised
+    # Load patent data
+    data_raw_df = patents.get_patents_from_s3()
+    # Load keywords
+    keywords = patents.get_keywords_from_s3()
+    # Process and filter the data
+    texts_df = (
+        data_raw_df
+        # Combine title and abstract
+        .assign(text=lambda df: df["title"] + ". " + df["abstract"])
+        # Remove patents without text
+        .dropna(subset=["text"])
+        # Check which patents have keyword hits in the same sentence
+        .assign(has_hits=lambda df: kw.check_keyword_hits(df.text, keywords))
+        .query("has_hits == True")
+        .rename(columns={"publication_number": "id"})
+    )[["text", "id"]]
+    # Remove texts that are already labelled
+    try:
+        labelled = load_jsonl(str(OUTPUT_FILEPATH / OUTPUT_FILENAME) + ".jsonl")
+        labelled_ids = pd.DataFrame(labelled).id.to_list()
+        texts_df = texts_df[~texts_df.id.isin(labelled_ids)]
+    except FileNotFoundError:
+        logging.info("No labelled data found")  # noqa: T001
+    # Subsample for testing
+    texts_df = texts_df.sample(100).reset_index(drop=True)
 
     # Fetch the categories
     categories = json.loads(PATH_TO_CATEGORIES.read_text())
@@ -71,7 +81,7 @@ async def main() -> None:
 
     message = MessageTemplate.load(str(PATH_TO_MESSAGE_PROMPT))
     function = FunctionTemplate.load(str(PATH_TO_FUNCTION))
-    examples = create_examples_string(load_jsonl(PATH_TO_PROMPTS / "examples.jsonl"))
+    examples = create_examples_string(load_jsonl(PATH_TO_EXAMPLES))
 
     model = "gpt-4-1106-preview"
     temperature = 0.0
@@ -104,8 +114,8 @@ async def main() -> None:
             result = await future  # Get the result (waits if not ready)
             await Classifier.write_line_to_file(
                 result,
+                OUTPUT_FILEPATH,
                 OUTPUT_FILENAME,
-                "test_data",
             )  # Write to the file
 
         time.sleep(2)
