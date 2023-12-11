928aa9d45c4fc2feb6d550b4623a58ceba89ffab
RFOxbury <rosamundoxbury@gmail.com>
2023-12-11 12:40:33 +0000

    Improve prodigy recipe
    * Options are presented in alphabetical order
    * Deduplication is implemented so that you can save your annotations and close the app, and when you start it up again, you will not be presented with examples that you have already labelled.

--- discovery_child_development/notebooks/labelling/prodigy/taxonomy_classifier_recipe.py
+++ discovery_child_development/notebooks/labelling/prodigy/taxonomy_classifier_recipe.py
@@ -5,12 +5,12 @@ prodigy oa_classification taxonomy_data discovery_child_development/notebooks/la
 ```
 or
 ```
-prodigy oa_classification taxonomy_data inputs/data/labelling/taxonomy/training_validation_data.jsonl -F discovery_child_development/notebooks/labelling/prodigy/taxonomy_classifier_recipe.py
+prodigy oa_classification taxonomy_data inputs/data/labelling/taxonomy/input/training_validation_data.jsonl -F discovery_child_development/notebooks/labelling/prodigy/taxonomy_classifier_recipe.py
 ```
 
 To export the data and have it saved locally, run:
 ```
-prodigy db-out taxonomy_data > discovery_child_development/notebooks/labelling/prodigy/taxonomy_data.jsonl
+prodigy db-out taxonomy_data > inputs/data/labelling/taxonomy/output/training_validation_data_LABELLED.jsonl
 ```
 
 If you have labelled your test examples and want to scrap those labels and start again (eg if you've switched to a different GPT model),
@@ -22,6 +22,7 @@ prodigy drop taxonomy_data
 """
 
 import prodigy
+from prodigy.components.db import connect
 from prodigy.components.loaders import JSONL
 from pathlib import Path
 from typing import Iterator
@@ -50,48 +51,63 @@ encoding = tiktoken.encoding_for_model(MODEL)
 
 categories_flat = tlu.load_categories()
 
+categories_list = sorted(list(categories_flat.keys()))
 
-def make_tasks(stream: Iterator[dict], model=MODEL) -> Iterator[dict]:
+
+def get_existing_ids(dataset_name, id_field="id"):
+    db = connect()
+    return {
+        example[id_field]
+        for example in db.get_dataset(dataset_name)
+        if id_field in example
+    }
+
+
+def make_tasks(
+    stream: Iterator[dict], existing_ids: set, model=MODEL
+) -> Iterator[dict]:
     for eg in stream:
-        task = copy.deepcopy(eg)
-        text = eg["text"]
+        if eg.get("id") in existing_ids:
+            continue
+        else:
+            task = copy.deepcopy(eg)
+            text = eg["text"]
 
-        # Format the prompt with the text to be classified
-        prompt = tlu.build_prompt(text, categories_flat)
+            # Format the prompt with the text to be classified
+            prompt = tlu.build_prompt(text, categories_flat)
 
-        function = tlu.format_function(categories_flat)
+            function = tlu.format_function(categories_flat)
 
-        # Call OpenAI API
-        response = client.chat.completions.create(
-            model=model,
-            temperature=TEMPERATURE,
-            messages=prompt,
-            functions=[function],
-            function_call={"name": "predict_category"},
-        )
+            # Call OpenAI API
+            response = client.chat.completions.create(
+                model=model,
+                temperature=TEMPERATURE,
+                messages=prompt,
+                functions=[function],
+                function_call={"name": "predict_category"},
+            )
 
-        # Process and format the output for Prodigy
-        options = [
-            {"id": category, "text": category}
-            for category in list(categories_flat.keys())
-        ]
+            # Process and format the output for Prodigy
+            options = [
+                {"id": category, "text": category} for category in categories_list
+            ]
 
-        output_as_list = tlu.get_labels_from_gpt_response(response)
+            output_as_list = tlu.get_labels_from_gpt_response(response)
 
-        task["tokens_input"] = response.usage.prompt_tokens
-        task["tokens_output"] = response.usage.completion_tokens
+            task["tokens_input"] = response.usage.prompt_tokens
+            task["tokens_output"] = response.usage.completion_tokens
 
-        task["cost"] = (MODEL_INPUT_COST * (response.usage.prompt_tokens / 1000)) + (
-            MODEL_OUTPUT_COST * (response.usage.completion_tokens / 1000)
-        )
+            task["cost"] = (
+                MODEL_INPUT_COST * (response.usage.prompt_tokens / 1000)
+            ) + (MODEL_OUTPUT_COST * (response.usage.completion_tokens / 1000))
 
-        task["options"] = options
+            task["options"] = options
 
-        task["accept"] = output_as_list
+            task["accept"] = output_as_list
 
-        task["model_output"] = output_as_list
+            task["model_output"] = output_as_list
 
-        yield task
+            yield task
 
 
 @prodigy.recipe(
@@ -100,9 +116,12 @@ def make_tasks(stream: Iterator[dict], model=MODEL) -> Iterator[dict]:
     source=("The source data as a .jsonl file", "positional", None, Path),
 )
 def custom_oa(dataset: str, source: str):
+    # Get existing IDs from the dataset
+    existing_ids = get_existing_ids(dataset)
+
     stream = JSONL(source)
 
-    stream = make_tasks(stream, model=MODEL)
+    stream = make_tasks(stream, existing_ids, model=MODEL)
 
     return {
         "dataset": dataset,
