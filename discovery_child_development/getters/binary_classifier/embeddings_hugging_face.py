938815aae6bf96597a6fb4f5bdb785c47597ee47
jessgillam <jess.gillam@nesta.org.uk>
2023-12-14 16:25:04 +0000

    Adding setup for relevance classifier pipeline/test cases

---
+++ discovery_child_development/getters/binary_classifier/embeddings_hugging_face.py
@@ -0,0 +1,43 @@
+from nesta_ds_utils.loading_saving import S3
+from discovery_child_development import config, S3_BUCKET
+
+VECTORS_PATH = "data/openAlex/vectors/distilbert/"
+VECTORS_FILE = "sentence_vectors_384"
+TRAINING_SETS = ["20", "50", "all"]
+
+
+def get_embeddings(
+    identifier="all",
+    bucket=S3_BUCKET,
+    vectors_path=VECTORS_PATH,
+    vectors_file=VECTORS_FILE,
+    production=True,
+    set_type="train",
+):
+    """Downloads OpenAlex embeddings for the training set from S3. Contains all the data used
+    for training the simple binary classifier.
+
+    Args:
+        identifier (str, optional): The identifier for the training data. Defaults to "all". Options are "50", "20", "all".
+        bucket (str, optional): Name of the bucket where the data is stored. Defaults to S3_BUCKET.
+        vectors_path (str, optional): The path to the vectors. Defaults to VECTORS_PATH.
+        vectors_file (str, optional): The name of the vectors file. Defaults to VECTORS_FILE.
+        production (bool, optional): Whether to use the production data. Defaults to True.
+        set_type (str, optional): Whether to use the training/validation/testing set. Defaults to "train". Options are "train", "validation".
+
+    Returns:
+        pandas.DataFrame: Dictionary with following keys:
+            - openalex_id (str): the id of the paper eg "https://openalex.org/W4249228678"
+            - input_ids (list): the input ids of the paper
+            - attention_mask (list): the attention mask of the paper
+            - label (int): the label of the paper
+    """
+    if production:
+        embedding_filename = f"{vectors_file}_{identifier}_{set_type}.pkl"
+    else:
+        embedding_filename = f"{vectors_file}_test_{identifier}_{set_type}.pkl"
+
+    openalex_data = S3.download_obj(
+        bucket, path_from=f"{vectors_path}{embedding_filename}"
+    )
+    return openalex_data
---
+++ discovery_child_development/utils/general_utils.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import tarfile
+import os.path
+from pathlib import Path
+
+
+def ensure_path_exists(path):
+    """Ensures that a path exists. If it doesn't, it creates it.
+
+    Args:
+        path (str): Path to be checked.
+    """
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+
+
+def replace_binary_labels(
+    df: pd.DataFrame,
+    label_col: str = "labels",
+    replace_cat: list = ["relevant", "not relevant"],
+):
+    """Replaces the labels in the dataframe with 1 (relevant) and 0 (not relevant).
+
+    Args:
+        df (pd.DataFrame): Dataframe to replace labels in.
+        label_col (str, optional): Column that contains the labels. Defaults to "labels".
+        replace_cat (list, optional): Categories to replace (order relevant first). Defaults to ["relevant", "not relevant"].
+
+    Returns:
+        pd.DataFrame: Dataframe with replaced labels.
+    """
+    df[label_col] = df[label_col].replace({replace_cat[0]: 1, replace_cat[1]: 0})
+    return df
+
+
+def make_tarfile(output_filename, source_dir):
+    """Creates a tarfile from a folder
+
+    Args:
+        output_filename (str): Name of the file to be saved
+        source_dir (str): Path to the folder to be zipped
+
+    Returns:
+        None: Saves the tarfile locally
+    """
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
