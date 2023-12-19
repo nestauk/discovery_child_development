955694de9ff930d964b26338e1e778fdef1a1c3e
jessgillam <jess.gillam@nesta.org.uk>
2023-12-19 09:48:34 +0000

    Adding models results for the relevant crunchbase data

--- discovery_child_development/getters/binary_classifier/model_results.py
+++ discovery_child_development/getters/binary_classifier/model_results.py
@@ -35,3 +35,33 @@ def get_openalex_results(
         kwargs_reading={"index_col": 0},
     )
     return openalex_data
+
+
+def get_crunchbase_results(
+    bucket: str = S3_BUCKET,
+    path_from: str = PATH_FROM,
+):
+    """Downloads the results for a sample of relevant crunchbase data from S3, using the huggingface pipeline on the GPT labelled data.
+    See discovery_child_development/pipeline/models/binary_classifier
+
+    Args:
+        bucket (str, optional): Name of the bucket where the data is stored. Defaults to S3_BUCKET.
+        path_from (str, optional): Path to the data. Defaults to PATH_FROM.
+        sample_size (int, optional): Number of samples from each of relevant/not relevant samples. Defaults to 500.
+
+    Returns:
+        pandas.DataFrame: A pandas dataframe with following columns:
+            - id (str): the crunchbase id
+            - text (str): the long description of the crunchbase work
+            - labels (str): the label of the paper eg 1:"Relevant" or 0:"Not-relevant"
+            - prediction (str): the prediction of the model eg 1:"Relevant" or 0:"Not-relevant"
+
+    """
+    filename = f"gpt_labelled_results_crunchbase.csv"
+    openalex_data = S3.download_obj(
+        bucket,
+        path_from=f"{path_from}{filename}",
+        download_as="dataframe",
+        kwargs_reading={"index_col": 0},
+    )
+    return openalex_data
