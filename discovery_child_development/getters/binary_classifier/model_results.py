11ae1e8017d39142b0e961cd61cce3e1b97e5249
j-gillam <107113232+j-gillam@users.noreply.github.com>
2023-12-21 17:19:48 +0000

    [56] Getter for relevant crunchbase sample for labelling (#57)
    * Adding models results for the relevant crunchbase data
    
    * Updating save path

--- discovery_child_development/getters/binary_classifier/model_results.py
+++ discovery_child_development/getters/binary_classifier/model_results.py
@@ -1,7 +1,7 @@
 from nesta_ds_utils.loading_saving import S3
 from discovery_child_development import config, S3_BUCKET
 
-PATH_FROM = "data/openAlex/test_text/"
+PATH_FROM = "data/labels/binary_classifier/test_text/"
 
 
 def get_openalex_results(
@@ -28,6 +28,36 @@ def get_openalex_results(
 
     """
     filename = f"gpt_labelled_results_sample_size_{sample_size}.csv"
+    openalex_data = S3.download_obj(
+        bucket,
+        path_from=f"{path_from}{filename}",
+        download_as="dataframe",
+        kwargs_reading={"index_col": 0},
+    )
+    return openalex_data
+
+
+def get_crunchbase_results(
+    bucket: str = S3_BUCKET,
+    path_from: str = PATH_FROM,
+):
+    """Downloads the results for a sample of relevant crunchbase data from S3, using the huggingface pipeline on the GPT labelled data.
+    See discovery_child_development/pipeline/models/binary_classifier
+
+    Args:
+        bucket (str, optional): Name of the bucket where the data is stored. Defaults to S3_BUCKET.
+        path_from (str, optional): Path to the data. Defaults to PATH_FROM.
+        sample_size (int, optional): Number of samples from each of relevant/not relevant samples. Defaults to 500.
+
+    Returns:
+        pandas.DataFrame: A pandas dataframe with following columns:
+            - id (str): the crunchbase id
+            - text (str): the long description of the crunchbase work
+            - labels (str): the label of the paper eg 1:"Relevant" or 0:"Not-relevant"
+            - prediction (str): the prediction of the model eg 1:"Relevant" or 0:"Not-relevant"
+
+    """
+    filename = f"gpt_labelled_results_crunchbase.csv"
     openalex_data = S3.download_obj(
         bucket,
         path_from=f"{path_from}{filename}",
