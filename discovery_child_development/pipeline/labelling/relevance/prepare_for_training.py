f9e9f85f6480066070756b54554e2d12d84b12d1
beingkk <kandersk@gmail.com>
2024-01-04 18:01:44 +0000

    adding the training dataset script

--- discovery_child_development/pipeline/labelling/relevance/prepare_for_training.py
+++
@@ -1,7 +0,0 @@
-"""
-Script to prepare the data for training the relevance model.
-
-Usage:
-"""
-
-## To do - take code from 00_relevancy_labels_testing.ipynb and add it here for collating and creating a csv file of the labels + text
--- discovery_child_development/pipeline/labelling/relevance/relevance_labels.py
+++ discovery_child_development/pipeline/labelling/relevance/relevance_labels.py
@@ -1,8 +1,8 @@
 """
 This script is used to test the relevancy labelling prompt and function.
 
-Usage:
-    python discovery_child_development/pipeline/labelling/relevance/relevance_labels.py --dataset openalex --num_samples 5 --model gpt-4-1106-preview
+Example usage:
+    python discovery_child_development/pipeline/labelling/relevance/relevance_labels.py --dataset openalex --num_samples 5 --model gpt-3.5-turbo-1106 --output_filename testing
 """
 import argparse
 import json
@@ -13,7 +13,7 @@ from pathlib import Path
 
 from nesta_ds_utils.loading_saving import S3
 from discovery_child_development import logging, PROJECT_DIR, S3_BUCKET
-from discovery_child_development.getters import openalex, patents
+from discovery_child_development.getters import get_dataset
 from discovery_child_development.utils.openai_utils import (
     MessageTemplate,
     Classifier,
@@ -39,7 +39,7 @@ PATH_TO_MESSAGE_PROMPT = PATH_TO_PROMPTS / "prompt.json"
 PATH_TO_FUNCTION = PATH_TO_PROMPTS / "function.json"
 PATH_TO_EXAMPLES = PATH_TO_PROMPTS / "examples.jsonl"
 # Define paths to the outputs
-OUTPUT_FILEPATH = PROJECT_DIR / "outputs/labels/relevance"
+OUTPUT_FILEPATH = PROJECT_DIR / CONFIG["local_output_directory"]
 
 
 async def main(
@@ -49,24 +49,19 @@ async def main(
     temperature: float,
     output_filename: str,
     s3_path: str,
-    use_s3: bool,
 ) -> None:
     """Fetch prompts and run the classifier"""
 
     # Fetch the data to be categorised
-    if dataset == "openalex":
-        texts_df = openalex.get_abstracts()
-    elif dataset == "patents":
-        texts_df = patents.get_and_process_patents_from_s3()
-    # Remove texts that are already labelled
+    texts_df = get_dataset(dataset)
+    # Remove datapoints that are already labelled
     try:
-        if use_s3:
-            # Update the outputs file with the S3 version
-            S3.download_file(
-                path_from=s3_path,
-                bucket=S3_BUCKET,
-                path_to=str(OUTPUT_FILEPATH / output_filename) + ".jsonl",
-            )
+        # Update the outputs file with the S3 version
+        S3.download_file(
+            path_from=s3_path,
+            bucket=S3_BUCKET,
+            path_to=str(OUTPUT_FILEPATH / output_filename) + ".jsonl",
+        )
         labelled = load_jsonl(str(OUTPUT_FILEPATH / output_filename) + ".jsonl")
         labelled_ids = pd.DataFrame(labelled).id.to_list()
         texts_df = texts_df[~texts_df.id.isin(labelled_ids)]
@@ -124,12 +119,11 @@ async def main(
         time.sleep(2)
 
     # Upload to S3
-    if use_s3:
-        S3.upload_file(
-            path_from=str(OUTPUT_FILEPATH / output_filename) + ".jsonl",
-            bucket=S3_BUCKET,
-            path_to=s3_path,
-        )
+    S3.upload_file(
+        path_from=str(OUTPUT_FILEPATH / output_filename) + ".jsonl",
+        bucket=S3_BUCKET,
+        path_to=s3_path,
+    )
 
 
 def parse_arguments():
@@ -141,7 +135,6 @@ def parse_arguments():
     parser.add_argument("--model", type=str, help="The model")
     parser.add_argument("--num_samples", type=int, help="The number of samples")
     parser.add_argument("--output_filename", type=str, help="The output filename")
-    parser.add_argument("--use_s3", type=bool, help="Whether to use S3")
     # Parse the arguments
     return parser.parse_args()
 
@@ -152,14 +145,15 @@ if "__main__" == __name__:
     try:
         dataset = args.dataset
     except ValueError as e:
-        raise ValueError("Dataset must be one of 'patents' or 'openalex'")
+        raise ValueError(
+            "Dataset must be one of 'patents', 'openalex' or 'openalex_broad'"
+        )
     num_samples = args.num_samples if args.num_samples else CONFIG["num_samples"]
     model = args.model if args.model else CONFIG["model"]
     temperature = CONFIG["temperature"]
     output_filename = (
         args.output_filename if args.output_filename else CONFIG["output_filename"]
     )
-    use_s3 = args.use_s3 if args.use_s3 else True
 
     # Create outputs directory if it doesn't exist
     create_directory_if_not_exists(OUTPUT_FILEPATH)
@@ -181,7 +175,6 @@ if "__main__" == __name__:
                 temperature,
                 output_filename,
                 s3_path,
-                use_s3,
             )
         )
     finally:
