e383cb1826cb174897479a12e5d4ab35c6517c5b
Karlis Kanders <beingkk@users.noreply.github.com>
2024-01-23 15:39:18 +0000

    [63] Generate detection-management labels (#64)
    * Move notebooks to subdirectory
    
    * Insert full stop between title and abstract for OpenAlex data
    
    * Change average to macro; add getter
    
    Averaging using "macro" gives all classes equal importance, so your metrics aren't skewed towards larger classes.
    
    * Add notebook exploring model performance on different classes
    
    * Update baseline model in order to log confusion matrix
    
    * Remove duplicate script
    
    * Add correction to filepath in baseline model
    
    * Tweak what is logged on wandb
    
    * Fix how metrics are averaged
    
    * Track knn, one vs rest and random forest on wandb
    
    * Update requirements
    
    * Add fine-tuning notebook
    
    * WIP - expanding openalex via keywords
    
    * WIP: OpenAlex metaflow to use keyword search
    
    * Search abstract and title only, not full works.
    
    * WIP: update notebook:
    
    * Squish keywords into one api query
    
    * Explore new OpenAlex data and refactor
    
    * Update openai version
    
    * Update OpenAlex EDA
    
    * Add notebook to git
    
    * Update OpenAlex metaflow to save data with timestamp and metadata
    
    * WIP: trying to fix OpenAlex clustering
    
    * Version OpenAlex data in the concepts metaflow
    
    * Update process for preprocessing openalex data
    
    * Update OpenAlex pipeline for taxonomy classifier
    
    * Add docstrings and types
    
    * Revert cluster analysis utils
    
    * Adjusting prompts
    
    * Adjusting prompts
    
    * labelling patents
    
    * Update docstring
    
    * Fix minor bug in getter
    
    * labelling 1000+ samples
    
    * adjusting column order
    
    * starting to refactor
    
    * refactoring
    
    * uploading/downloading labels to s3
    
    * adding the training dataset script
    
    * cleaning up
    
    * adding readme and simplifying the pipeline
    
    * renaming the notebook
    
    * edit gitignore
    
    * readme
    
    * adding docstrings
    
    * refactoring
    
    * changed s3 location
    
    * refactoring
    
    * define allowed datasets
    
    * refactoring
    
    * cleaning up
    
    * refactoring
    
    * add labels getters
    
    * refactoring
    
    * refactoring
    
    * adjusting docstrings
    
    * implementing review feedback
    
    * cleaning up
    
    * building prompts and pipeline
    
    * removing data file
    
    * bug: fixing jsonl downwload bug
    
    * building prompts and pipeline
    
    * adjusing config
    
    * cleaning up
    
    * cleaning up
    
    * reducing batch size
    
    * refactor: remove unused argparse
    
    * chore: gitignore
    
    * chore: gitignore
    
    * fix: fixed empty folder creation
    
    * chore: checking results
    
    * fix: labels in examples
    
    ---------
    
    Co-authored-by: RFOxbury <rosamundoxbury@gmail.com>

--- discovery_child_development/getters/labels.py
+++ discovery_child_development/getters/labels.py
@@ -24,12 +24,15 @@ def get_relevance_labels() -> pd.DataFrame:
         PROJECT_DIR
         / "discovery_child_development/pipeline/labelling/relevance/config.yaml"
     )
-    local_path = f'{config["local_output_directory"]}/{config["output_filename"]}.jsonl'
+    local_path = str(
+        PROJECT_DIR
+        / f'{config["local_output_directory"]}/{config["output_filename"]}.jsonl'
+    )
     Path(local_path).parent.mkdir(parents=True, exist_ok=True)
     data = jsonl_utils.download_file_from_s3(
         bucket_name=S3_BUCKET,
         s3_file_name=f'{config["s3_directory"]}{config["output_filename"]}.jsonl',
-        local_file=f"{str(PROJECT_DIR)}/{local_path}",
+        local_file=local_path,
     )
     return pd.DataFrame(data)
 
@@ -54,16 +57,16 @@ def get_taxonomy_labels(raw: bool = False) -> pd.DataFrame:
     """
     config = get_yaml_config(
         PROJECT_DIR
-        / "discovery_child_development/pipeline/labelling/taxonomy_v2/config.yaml"
+        / "discovery_child_development/pipeline/labelling/taxonomy/config.yaml"
     )
     if raw:
         fname = config["output_filename"] + "_raw.jsonl"
-        local_path = f'{config["local_output_directory"]}/{fname}'
+        local_path = str(PROJECT_DIR / f'{config["local_output_directory"]}/{fname}')
         Path(local_path).parent.mkdir(parents=True, exist_ok=True)
         data = jsonl_utils.download_file_from_s3(
             bucket_name=S3_BUCKET,
             s3_file_name=f'{config["s3_directory"]}{fname}',
-            local_file=f"{str(PROJECT_DIR)}/{local_path}",
+            local_file=local_path,
         )
         return pd.DataFrame(data)
     else:
@@ -72,3 +75,34 @@ def get_taxonomy_labels(raw: bool = False) -> pd.DataFrame:
             path_from=f'{config["s3_directory"]}{config["output_filename"]}.parquet',
             download_as="dataframe",
         )
+
+
+def get_detection_management_labels() -> pd.DataFrame:
+    """Get relevance labels from S3
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the relevance labels.
+            Columns are:
+                - id: The ID of the work
+                - source: The source of the data (so far: openalex or patent)
+                - text: The text of the work (title + abstract)
+                - prediction: Relevant (is about preschool-age child development),
+                    Not-relevant, Not-specified (might be about child development but age unclear)
+                - model: The model used to make the prediction
+                - timestamp: The timestamp of the prediction
+    """
+    config = get_yaml_config(
+        PROJECT_DIR
+        / "discovery_child_development/pipeline/labelling/detection_management/config.yaml"
+    )
+    local_path = str(
+        PROJECT_DIR
+        / f'{config["local_output_directory"]}/{config["output_filename"]}.jsonl'
+    )
+    Path(local_path).parent.mkdir(parents=True, exist_ok=True)
+    data = jsonl_utils.download_file_from_s3(
+        bucket_name=S3_BUCKET,
+        s3_file_name=f'{config["s3_directory"]}{config["output_filename"]}.jsonl',
+        local_file=local_path,
+    )
+    return pd.DataFrame(data)
