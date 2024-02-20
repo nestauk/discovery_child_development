69f57f8c72048df3de13de50e56d68f307294371
beingkk <kandersk@gmail.com>
2024-02-20 14:01:11 +0000

    feat: relevance inference across all data

---
+++ discovery_child_development/pipeline/README.md
@@ -0,0 +1,39 @@
+# Pipelines for data downloads and processing
+
+## OpenAlex publication data
+
+### Downloading from OpenAlex API
+
+To download OpenAlex data run the following two commands.
+
+Download papers tagged with relevant concepts (tags)
+
+```bash
+python discovery_child_development/pipeline/openalex/00_openalex_metaflow.py run --production True
+```
+
+The outputs will be saved in s3 in metaflow/openalex_concepts. Note that the results from this run might be distributed across multiple folders with slightly different timestamps - this has to do with some steps failing and then being retried with later timestamps.
+
+Download papers with the relevant keywords
+
+```bash
+python discovery_child_development/pipeline/openalex/openalex_keyword_search.py run --production True
+```
+
+The outputs from this run will be saved in metaflow/openalex_keyword_search folders
+
+### Preprocessing OpenAlex data
+
+The next step is some light preprocessing of the OpenAlex data. The following script takes data from the metaflow folder and outputs it into data/openAlex/openalex_works_concepts_YYYYMMDD_HHMMSS/ with the timestamp indicating the time of the run.
+
+```bash
+python discovery_child_development/pipeline/openalex/01_preprocess_openalex.py
+```
+
+## Patents data download
+
+Patent data is donwloaded with the following query
+
+```bash
+python discovery_child_development/pipeline/patents/00_fetch_patents.py
+```
---
+++ discovery_child_development/pipeline/models/binary_classifier/05_inference_OpenAlex_Patents.py
@@ -0,0 +1,95 @@
+"""
+Run the inference pipeline.
+
+Usage:
+
+python discovery_child_development/pipeline/models/binary_classifier/05_inference.py
+
+
+"""
+import pandas as pd
+import numpy as np
+from discovery_child_development import (
+    PROJECT_DIR,
+    binary_config,
+    config,
+    S3_BUCKET,
+    labelling_config,
+    logging,
+)
+from nesta_ds_utils.loading_saving import S3
+from discovery_child_development.utils.huggingface_pipeline import (
+    load_model,
+    load_training_args,
+    load_trained_model,
+)
+from discovery_child_development.getters.binary_classifier.binary_classifier_model import (
+    get_binary_classifier_models,
+)
+from discovery_child_development.utils.huggingface_pipeline import (
+    predictions_huggingface,
+)
+from discovery_child_development.getters.unlabelled_data import (
+    get_data_for_relevance_classifier,
+)
+
+from discovery_child_development.getters import patents, openalex
+
+# Model vars
+production = True
+
+# Set the seed
+SEED = config["seed"]
+np.random.seed(SEED)
+
+# Paths
+S3_PATH = "models/binary_classifier/"
+PATH_TO = f"{PROJECT_DIR}/outputs/data/models/"
+MODEL_FILENAME = (
+    f"gpt_labelled_binary_classifier_distilbert_production_{production}.tar.gz"
+)
+OUTPUT_FILENAME = labelling_config["OUTPUT_FILENAME"]
+
+if __name__ == "__main__":
+    # Save the model locally
+    logging.info("Downloading the model...")
+    get_binary_classifier_models(
+        filename=MODEL_FILENAME, s3_path=S3_PATH, path_to=PATH_TO
+    )
+    model_folder = (
+        f"{PATH_TO}gpt_labelled_binary_classifier_distilbert_production_{production}"
+    )
+
+    # Load the model
+    logging.info("Loading the model...")
+    model = load_model(model_path=model_folder, config=binary_config, num_labels=2)
+
+    # Train model with early stopping
+    training_args = load_training_args(**binary_config["training_args"])
+    trainer = load_trained_model(
+        model=model,
+        args=training_args,
+        config=binary_config,
+    )
+
+    # Get the labelled data
+    logging.info("Getting the labelled data...")
+    patents_df = patents.get_and_process_patents_from_s3()
+    openalex_df = openalex.get_abstracts()
+    data_for_labelling = pd.concat(
+        [patents_df[["id", "text"]], openalex_df[["id", "text"]]], ignore_index=True
+    )
+
+    # Get the predictions
+    logging.info("Getting the predictions...")
+    predictions = predictions_huggingface(
+        trainer=trainer, text_data=data_for_labelling, config=binary_config
+    )
+
+    # Save the predictions
+    logging.info("Saving the predictions...")
+    S3.upload_obj(
+        predictions,
+        S3_BUCKET,
+        f"data/outputs/binary_classifier/{OUTPUT_FILENAME}.csv",
+    )
