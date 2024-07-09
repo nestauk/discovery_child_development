ab78b50acb13b7488c95ea16325195f7af2777d4
beingkk <kandersk@gmail.com>
2024-07-09 16:23:50 +0100

    adding password to assistant

--- discovery_child_development/assistant/app_ai_soft_play.py
+++ discovery_child_development/assistant/app_ai_soft_play.py
@@ -55,6 +55,36 @@ def retrieve_similar_vectors(
 possible_topics = ["openalex", "patents", "crunchbase", "gtr"]
 
 
+def check_password() -> bool:
+    """Return `True` if the user had the correct password."""
+
+    def password_entered() -> None:
+        """Check whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+
+        return True
+
+
 # Streamlit app
 def main():
     # Load in variables in the session state, to persist during the usage of the app
@@ -172,4 +202,5 @@ def main():
         st.write(st.session_state["summary"])
 
 
-main()
+if check_password():
+    main()
